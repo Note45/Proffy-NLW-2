ec4e98ea7f323d0ebe8cc60cdb5f6dc4cb0c3d5b
Note45 <antonion.2048@gmail.com>
2020-08-05 20:02:28 -0300

    Connected front-end to back-end and finished the web application

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,32 +1,58 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
+  
   return(
     <article className="teacher-item">
       <header>
-        <img src='https://avatars3.githubusercontent.com/u/41867280?s=460&u=474a02c10c9d59781e35653980047069187a43f8&v=4' alt='Avatar'/>
+        <img src={teacher.avatar} alt='Avatar'/>
 
         <div>
-          <strong>Antonio</strong>
+          <strong>{teacher.name}</strong>
           <span>Programação Estruturada</span>
         </div>
       </header>
 
-      <p>Description</p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/Hora
         </p>
-        <strong>R$ 100,00</strong>
-        <button type='button'>
+        <strong>{`R$ ${teacher.cost}`}</strong>
+        <a 
+          target='_blank' 
+          rel="noopener noreferrer" 
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt='Entrar em contato'/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
